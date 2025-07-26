844a83f914f9bb4f8553923bf2b942aa789d75b1
Yuvaraj <yuvarajrayala89@gmail.com>
2025-07-26 21:17:14 +0530

    second commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -33,7 +33,7 @@ const HomePage = () => {
     <div className="container">
       {/* Header */}
       <div className="header">
-        <h1>E-Commerce</h1>
+        <h1>E-Commerce KLU App</h1>
         <div className="header-right">
           {isAuthenticated ? (
             <div className="profile-menu">
